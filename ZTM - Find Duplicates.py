17c595883ad4f8a6f25807659dba9cc800aaf657
Angelis <48072083+Angelion879@users.noreply.github.com>
2021-02-10 10:30:03 -0300

    Create ZTM - Find Duplicates.py

---
+++ ZTM - Find Duplicates.py
@@ -0,0 +1,10 @@
+some_list = ['a','b','c','b','d','m','n','n']
+duplicates = []
+
+for item in some_list:
+    num = some_list.count(item)
+    if num > 1 and item not in duplicates:
+        duplicates.append(item)
+    else:
+        continue
+print(duplicates)
