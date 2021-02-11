3cac42dad127bc017712e9c41adac80dff0359e9
Angelis <48072083+Angelion879@users.noreply.github.com>
2021-02-11 11:43:15 -0300

    Delete ZTM - Password Checker.py

--- ZTM - Password Checker.py
+++
@@ -1,6 +0,0 @@
-user_name = input('Whats your user? ')
-password = input('Whats your password? ')
-password_lenght = len(password)
-secret = '*' * password_size
-
-print(f'{user_name}, your password {secret} is {password_size} characters long')
