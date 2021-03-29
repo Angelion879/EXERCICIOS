6c2ea0b265cc230bf2d2d520953df8de718fbe24
Angelis <48072083+Angelion879@users.noreply.github.com>
2021-03-29 17:14:02 -0300

    more testing

--- Triple_x/triple_x.cpp
+++ Triple_x/triple_x.cpp
@@ -9,5 +9,17 @@ int main()
     std::cout << "The computer has several firewalls, insert the right combination of numbers to pass trough.";
     std::cout << std::endl;
     std::cout << "Good luck, Agent. We're counting on you.";
+
+    int a = 5;
+    int b = 4;
+    int c = 6;
+
+    int sum = a + b + c;
+    int mult = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << mult << std::endl;
+
     return 0;
 }
