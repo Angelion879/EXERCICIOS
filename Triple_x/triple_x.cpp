26e5f16f4798436896434158013df0eef3834d88
Angelis <48072083+Angelion879@users.noreply.github.com>
2021-03-29 15:12:51 -0300

    game update

--- Triple_x/triple_x.cpp
+++ Triple_x/triple_x.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
-    std::cout << "Hello World!";
+    std::cout << "Welcome, Agent. As you know, you are a Junior agent, about to get a promotion, so, you need to be tested:";
+    std::cout << std::endl;
+    std::cout << "Get into world's most evil mastermind computer and discover his next big plan.";
+    std::cout << std::endl;
+    std::cout << "The computer has several firewalls, insert the right combination of numbers to pass trough.";
+    std::cout << std::endl;
+    std::cout << "Good luck, Agent. We're counting on you.";
     return 0;
 }
